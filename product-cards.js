c0fd8de26b9a1578611d100161626833bd09a459
Sam <li.cw.samantha@gmail.com>
2025-06-05 03:00:09 +1000

    making sure everything is updated

--- product-cards.js
+++ product-cards.js
@@ -1,6 +1,6 @@
 const products = [
-  { name: "Echeveria", price: "$29.95", img: "images/kenny_pic.jpg" },
-  { name: "Angelica", price: "$189.95", img: "images/Angelica.png" },
+  { id: "1", name: "Echeveria", price: "$29.95", img: "images/kenny_pic.jpg" },
+  { id: "2", name: "Zayne", price: "$59.95", img: "images/Zayne.jpg" },
 ];
 
 const template = document.getElementById("product-card-template");
@@ -12,6 +12,10 @@ for (let i = 0; i < cards; i++) {
     for (let j = 0; j < cards; j++) {
         const product = products[(j+i) % products.length]; // alternate between 0 and 1
         const clone = template.content.cloneNode(true);
+
+        const link = clone.querySelector(".product-card-link");
+         link.href = `product.html?id=${product.id}`;
+
         clone.querySelector("img").src = product.img;
         clone.querySelector("img").alt = product.name;
         clone.querySelector(".product-name").textContent = product.name;
--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const products = [
-  { name: "Echeveria", price: "$29.95", img: "images/kenny_pic.jpg" },
-  { name: "Angelica", price: "$189.95", img: "images/Angelica.png" },
+  { id: "1", name: "Echeveria", price: "$29.95", img: "images/kenny_pic.jpg" },
+  { id: "2", name: "Zayne", price: "$59.95", img: "images/Zayne.jpg" },
 ];
 
 const template = document.getElementById("product-card-template");
@@ -9,11 +9,15 @@ const container = document.getElementById("product-list");
 const totalCards = 5;
 
 for (let i = 0; i < totalCards; i++) {
-  const product = products[i % products.length]; // alternate between 0 and 1
-  const clone = template.content.cloneNode(true);
-  clone.querySelector("img").src = product.img;
-  clone.querySelector("img").alt = product.name;
-  clone.querySelector(".product-name").textContent = product.name;
-  clone.querySelector(".product-price").textContent = product.price;
-  container.appendChild(clone);
+    const product = products[i % products.length]; // alternate between 0 and 1
+    const clone = template.content.cloneNode(true);
+
+    const link = clone.querySelector(".product-card-link");
+    link.href = `product.html?id=${product.id}`;
+
+    clone.querySelector("img").src = product.img;
+    clone.querySelector("img").alt = product.name;
+    clone.querySelector(".product-name").textContent = product.name;
+    clone.querySelector(".product-price").textContent = product.price;
+    container.appendChild(clone);
 }
